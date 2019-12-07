52c596bff51ec45999e6794d00d792a07e9a80b4
EricCWWong <ericwongchunwing@gmail.com>
2019-12-07 11:37:24 +0000

    added B-field

--- first_plot.py
+++ first_plot.py
@@ -5,26 +5,28 @@ from transmission import totalTransmission_n
 
 n = 5       # Number of steps
 V_sd = 0    # In units of h_bar w_x
+B = 3
+V_sd_steps = 5
 
 # setting the x - coordinates 
-x = np.arange(-1, 10, 0.1)    # In units of h_bar w_x
+x = np.arange(-10, 10, 0.1)    # In units of h_bar w_x
 
 
 
 fig, axs = plt.subplots(1, 5)
-fig.suptitle('The conductance of the material with a V_sd but no magnetic field')
+fig.suptitle('The conductance of the material (1.5)')
 axs[0].set_ylabel('G * h / 2e^2')
-axs[2].set_xlabel('(E_f - U_0) / h_bar w_x')
+axs[int(V_sd_steps/2 -0.5)].set_xlabel('(E_f - U_0) / h_bar w_x')
 
-for i in range (0, 5):
+for i in range (0, V_sd_steps):
     V_sd = i * 0.5
     # setting the corresponding y - coordinates
-    y = totalTransmission_n(n, x, V_sd)
+    y = 1/2 * totalTransmission_n(n, x, V_sd, B)
     axs[i].set_ylim([0, 5])
-    #axs[i].set_xlim([-1, 10])
-    
+    #axs[i].set_xlim([-1, 10])   
     axs[i].plot(x,y)
 
   
 # function to show the plot 
-plt.savefig('Conductance_without_B_field.pdf')
+#plt.show()
+plt.savefig('Conductance_with_Bfield2.pdf')
--- transmission.py
+++ transmission.py
@@ -4,23 +4,21 @@ import math
 # rename variables
 pi = np.pi
 
-
 def zeemanEnergy (S, B):
     g = 1
     mu_B = 1
     return g * mu_B * S * B
 
-
 # transmission coefficients without B field
 
-def transmissionForward (n, x, V_sd, B = 0, S = 1/2):
-    return 1/(1 + np.exp(-2 * pi * (x - (2 * n + 1) + 1 / 2 * V_sd + zeemanEnergy(1/2, 1)))) * 1 / 2
+def transmissionForward (n, x, V_sd, B, S):
+    return 1/(1 + np.exp(-2 * pi * (x - (2 * n + 1) + 1 / 2 * V_sd + zeemanEnergy(S, B)))) * 1 / 2
 
 def transmissionBackward (n, x, V_sd, B = 0, S = 1/2):
-    return 1/(1 + np.exp(-2 * pi * (x - (2 * n + 1) - 1 / 2 * V_sd + zeemanEnergy(1/2, 1)))) * 1 / 2
+    return 1/(1 + np.exp(-2 * pi * (x - (2 * n + 1) - 1 / 2 * V_sd + zeemanEnergy(S, B)))) * 1 / 2
 
-def totalTransmission_n (n, x, V_sd, B = 0):
+def totalTransmission_n (n, x, V_sd, B):
     output = 0
     for i in range (0, n + 1):
-        output = output + transmissionBackward(i, x, V_sd) + transmissionForward(i, x, V_sd)
+        output = output + transmissionBackward(i, x, V_sd, B, 1/2) + transmissionForward(i, x, V_sd, B, 1/2) + transmissionBackward(i, x, V_sd, B, - 1/2) + transmissionForward(i, x, V_sd, B, - 1/2)
     return output
