18756b124fb3f847146d4f7c88b24ba9d7e16f81
EricCWWong <ericwongchunwing@gmail.com>
2020-02-04 00:57:58 +0000

    Added some interesting results

--- run_save.sh
+++ run_save.sh
@@ -1,1 +1,1 @@
-gsimulator exp_setup.csv 5 --material material.csv --gname 'G with g=2' --save 'cheat_vary_Bv(g=2).pdf' --offset 2 > './Results/test.txt'
+gsimulator exp_setup.csv 5 --material material.csv --gname 'G with g=2,Vsd=2mV,angle=80 deg' --save 'Plot(varyB80deg,vsd=1mV).pdf' --offset 2 > './Results/Data(varyB80deg,vsd=1mV).txt'
