49ba098a001463a6bb87d61cde4ca2ad6323d771
Eric Wong <ericwongchunwing@gmail.com>
2020-03-13 16:40:07 +0000

    Added results

--- run_save.sh
+++ run_save.sh
@@ -1,1 +1,1 @@
-gsimulator exp_setup.csv 5 --material material.csv --gname 'Raw Conductance Graph' --save 'basic_conductance.pdf' --offset 0 > './Results/VaryB80deg,0mV.txt'
+gsimulator exp_setup.csv 5 --material material.csv --gname 'Vary 80 degrees B field with g=1.1 and Vsd=0mV' --save 'VaryB80deg(0mV,g=1.1).pdf' --offset 4 > './Results/Data(VaryB80deg,0mV,g=1.1).txt'
